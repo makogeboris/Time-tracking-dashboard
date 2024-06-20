4579277d53a16dc7678088689924743735c50276
makoge boris <makogeboris@gmail.com>
2024-06-20 14:31:34 +0100

    modified

--- script.js
+++ script.js
@@ -3,10 +3,10 @@
 const daily = document.getElementById("daily");
 const weekly = document.getElementById("weekly");
 const monthly = document.getElementById("monthly");
-const title = document.querySelectorAll(".category");
-const timeframe = document.querySelectorAll(".timeframe");
-const currentTime = document.querySelectorAll(".current-time");
-const previousTime = document.querySelectorAll(".previous-time");
+const title = document.querySelectorAll(".activity__title");
+const timeframe = document.querySelectorAll(".activity__current-timeframe");
+const currentTime = document.querySelectorAll(".activity__current-time");
+const previousTime = document.querySelectorAll(".activity__previous-time");
 const url = "data.json";
 
 fetch(url)
@@ -14,6 +14,9 @@ fetch(url)
   .then((data) => {
     console.log(data);
     addData(data);
+  })
+  .catch((err) => {
+    console.error(`Error fetching data: ${err.message}`);
   });
 
 function addData(data) {
@@ -29,3 +32,5 @@ function checkTimeframe() {
 }
 
 daily.addEventListener("click", checkTimeframe);
+
+//  The text for the previous period's time should change based on the active timeframe. For Daily, it should read "Yesterday" e.g "Yesterday - 2hrs". For Weekly, it should read "Last Week" e.g. "Last Week - 32hrs". For monthly, it should read "Last Month" e.g. "Last Month - 19hrs".
